e2a30c0a87f2e96443094438a57e8104f138f266
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2025-08-26 21:56:30 +0530

    Fixed Catalyst Deployment

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import v1Router from "./routes/v1";
 import "dotenv/config";
 const app = express();
 
+app.get("/", (req: Request, res: Response) => {
+  res.redirect("/v1/docs");
+});
+
 app.get("/health", (req: Request, res: Response) => {
   res.json({ status: "The server is healthy!" });
 });
@@ -13,8 +17,8 @@ app.use(express.json());
 
 app.use("/v1", v1Router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.X_ZOHO_CATALYST_LISTEN_PORT) || 3000;
 
-app.listen(3000, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`App is listening on port ${PORT}`);
 });
